aa7e9d7df8ccb20ef0a422d8239fcfc0d3027c3d
igorskyflyer <igor.dvlpr@gmail.com>
2019-10-19 20:01:09 +0200

    Add support for parallel execution

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const childProcess = require('child_process')
 
 /**
  * Synchronously runs the specified command.
+ * @since 1.0.0
  * @param {string} command Command to run.
  * @throws Will throw an error if no command is provided.
  * @returns {ExecResult} Returns the standard output.
@@ -34,6 +35,7 @@ function executeSync(command) {
 
 /**
  * Asynchronously, with a callback runs the specified command.
+ * @since 1.0.0
  * @param {string} command Command to run.
  * @param {ExecCallback} callback The function to call after the NPM function is executed.
  * @throws Will throw an error if no command is provided.
@@ -58,6 +60,7 @@ function executeCallback(command, callback) {
 
 /**
  * Asynchronously runs the specified command.
+ * @since 1.0.0
  * @param {string} command Command to run.
  * @throws Will throw an error if no command is provided.
  * @returns {Promise<string>}
@@ -74,4 +77,35 @@ async function execute(command) {
 	})
 }
 
-module.exports = { executeSync, executeCallback, execute }
+/**
+ * Asynchronously and in parallel runs the specified commands.
+ * @since 1.1.0
+ * @param {(...string|string[])} args Commands to run, either a string array, e.g. (['command', 'command', 'command'], ) or rest arguments, strings also, e.g. ('command', 'command', 'command')
+ * @throws Will throw an error if any of the commands is not defined.
+ * @returns {Promise<string[]>}
+ */
+async function executeParallel(args) {
+	if (!args) {
+		throw 'Error: No arguments provided.'
+	}
+
+	const commands = []
+
+	if (typeof args === 'string') {
+		const argsCount = arguments.length
+
+		for (let i = 0; i < argsCount; i++) {
+			commands.push(execute(arguments[i]))
+		}
+	} else if (args instanceof Array) {
+		const arrayCount = args.length
+
+		for (let i = 0; i < arrayCount; i++) {
+			commands.push(execute(args[i]))
+		}
+	}
+
+	return Promise.all(commands)
+}
+
+module.exports = { executeSync, executeCallback, execute, executeParallel }
